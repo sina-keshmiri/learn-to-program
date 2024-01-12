9d7f76911ed71e5162f19e1caa7eb5a13045db28
Sina Keshmiri <sina@fournine.digital>
2024-01-12 13:59:53 -0800

    feat: add solution binary and linear search functions

--- src/LTP-1/BinarySearchList.ts
+++ src/LTP-1/BinarySearchList.ts
@@ -6,5 +6,26 @@ Ensure that your solution has a time complexity of O(log n).
 IMPORTANT NOTE TO THINK ABOUT IS: SORTED ARRAY ex: arr = [1, 2, 3, 4, 5, 6, 7, 8, 10] target: 7
 */
 
+/*
+[1, 2, 3, 4, 5, 6, 7, 8, 10] target: 7
+^            ^             
+*/
+
 export default function bs_list(arr: number[], target: number): boolean {
+    let lo = 0
+    let hi = arr.length
+
+    while(lo < hi) {
+        const m = Math.floor(lo + (hi - lo) / 2)
+        const v = arr[m]
+
+        if (v === target) return true
+        else if (v > target) {
+            hi = m
+        } else {
+            lo = m + 1
+        }
+    }
+
+    return false
 }
--- src/LTP-1/LinearSearchList.ts
+++ src/LTP-1/LinearSearchList.ts
@@ -4,4 +4,9 @@ a target number ('target'). The function should return 'true' if the target numb
 */
 
 export default function linear_search(arr: number[], target: number): boolean {
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] === target) return true
+    }
+
+    return false
 }
